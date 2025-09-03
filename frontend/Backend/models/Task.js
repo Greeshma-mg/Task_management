b94cd60d0246f070a3c0a6f910f57b2751a9d668
Greeshma-mg <greeshmamgnair@gmail.com>
2025-09-03 18:08:53 +0530

    Clean structure: frontend fixed, removed duplicate folders

--- frontend/Backend/models/Task.js
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose";
-
-const taskSchema = new mongoose.Schema({
-  title: { type: String, required: true },
-  status: { type: String, default: "pending" },
-  userId: { type: mongoose.Schema.Types.ObjectId, ref: "User" }
-});
-
-export default mongoose.model("Task", taskSchema);
